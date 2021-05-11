cc365e504b0f96551af4095e508e0e6b85080604
Galina1977-dev <galyapener@mail.ru>
2021-05-11 21:40:18 +0500

    создала header about

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,26 @@
 $(function () {
-  // Слайдер-карусель Обычный
-  // $(".slider__inner").slick({
-  //   infinite: true,
-  //   fade: true,
-  //   prevArrow: '<button type="button" class="slick-prev"></button>',
-  //   nextArrow: '<button type="button" class="slick-next"></button>',
-  // });
+  $(document).ready(function () {
+    $(".button").click(function () {
+      $(".pop-up").toggleClass("show");
+    });
+  });
 
-  // Анимация
+  $(document).ready(function () {
+    $(".popup").click(function () {
+      $(".pop").fadeIn(300);
+    });
+
+    $(".pop__close").click(function () {
+      $(".pop").fadeOut(300);
+    });
+  });
+
+  $(".menu-btn").click(function (event) {
+    $(".menu-btn, .menu__list ").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
+
+  $(".filter-style").styler();
+
   new WOW().init();
 });
